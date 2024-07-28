9e352408b4330896346669e4d72f7a3f8f707ad3
Sreenidhi <sreenidhii@vcu.edu>
2024-07-28 18:53:38 +0530

    Update enhanced_finance_tracker.py

--- enhanced_finance_tracker.py
+++ enhanced_finance_tracker.py
@@ -3,6 +3,7 @@
 import streamlit as st
 import importlib
 from auth import authenticate, create_user
+from pages import home
 
 def main():
     # Set background colors and font style for main page and sidebar
